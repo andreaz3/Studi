afa1874e5aa5cb37b2bb972a0dae5eb31ddbec75
Peter Pao-Huang <peter.paohuang@gmail.com>
2023-11-01 11:10:52 -0500

    first

---
+++ studi/studi/studiApp.swift
@@ -0,0 +1,17 @@
+//
+//  studiApp.swift
+//  studi
+//
+//  Created by Peter Pao-Huang on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct studiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
