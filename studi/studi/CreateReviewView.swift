c510c4d9fa4d5e21b161ee4c989e15408e6caaf9
alihusain01 <48800789+alihusain01@users.noreply.github.com>
2023-11-09 16:57:43 -0600

    template CreateReviewView finished

--- studi/studi/CreateReviewView.swift
+++ studi/studi/CreateReviewView.swift
@@ -8,13 +8,84 @@
 import SwiftUI
 
 struct CreateReviewView: View {
+    @State private var wifiStrength: Double = 0
+    @State private var noiseLevel: Double = 0
+    @State private var foodQuality: Double = 0
+    @State private var drinkQuality: Double = 0
+    @State private var additionalNotes: String = ""
+    
+    
     var body: some View {
-        Text("Create Review Page")
+            NavigationView {
+                VStack{
+                    HStack{
+                        NavigationLink(destination: ReviewsView()) { // Change to NavigationLink to navigate to ReviewsView
+                                            Image(systemName: "arrow.backward") // Use a system image for back arrow
+                                                .resizable()
+                                                .frame(width: 30, height: 30) // Adjust size as needed
+                                                .foregroundColor(.black)
+                                                .padding(.leading, 30)
+                                                .padding(.top, 5)
+                                        }
+                        Spacer()
+                        Image("logo")
+                        Spacer()
+                        NavigationLink(destination: ProfileView()) {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .foregroundColor(.black)
+                                .padding(.trailing, 30)
+                                .padding(.top, 5)
+                        }
+                    }
+                    Form {
+                        Section(header: Text("Cafe Paradiso")) {
+                            HStack {
+                                Image(systemName: "wifi")
+                                    .foregroundColor(.gray)
+                                Slider(value: $wifiStrength, in: 0...5, step: 1) {
+                                    Text("Wifi")
+                                }
+                            }
+                            HStack {
+                                Image(systemName: "speaker.wave.2")
+                                    .foregroundColor(.gray)
+                                Slider(value: $noiseLevel, in: 0...5, step: 1) {
+                                    Text("Noise")
+                                }
+                            }
+                            HStack {
+                                Image(systemName: "fork.knife")
+                                    .foregroundColor(.gray)
+                                Slider(value: $foodQuality, in: 0...5, step: 1) {
+                                    Text("Food")
+                                }
+                            }
+                            HStack {
+                                Image(systemName: "cup.and.saucer")
+                                    .foregroundColor(.gray)
+                                Slider(value: $drinkQuality, in: 0...5, step: 1) {
+                                    Text("Drinks")
+                                }
+                            }
+                            TextField("Additional Notes", text: $additionalNotes)
+                        }
+                        
+                        Button("Post Review") {
+                            // Handle the post review action
+                        }
+                    }
+                }
+                
+            }
+        }
     }
-}
 
 struct CreateReviewView_Previews: PreviewProvider {
     static var previews: some View {
         CreateReviewView()
     }
 }
+
+
